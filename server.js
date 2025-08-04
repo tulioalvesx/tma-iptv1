04fdef6d495d1ecea4f683604b767df450153f40
tulioalvex <tulioalvex@gmail.com>
2025-08-04 11:39:43 -0300

    Comportamento do Chatbot v2.3

--- server.js
+++ server.js
@@ -15,12 +15,6 @@ const DATA_DIR = path.join(__dirname, "data");
 const PUBLIC_DIR = path.join(__dirname, "public");
 
 app.use(express.json());
-
-// Health check endpoint para verificar se o servidor está vivo
-app.get('/health', (req, res) => {
-  res.send('OK');
-});
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(PUBLIC_DIR));
 
